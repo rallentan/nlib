acd364e3678c72b27fb5fe41b7abab611aaeeef9
Mark <Mark@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2010-11-18 03:28:00 +0000

    Merged changes from the v1.1 branch into the trunk.

---
+++ NUnitTests.All/NLib_NET2/Int64ExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnitTests.NLib_NET2
+{
+    [TestFixture]
+    public class Int64ExtensionsTests
+    {
+        //--- Fields ---
+
+        NUnitTests.Int64ExtensionsTests testObject = new NUnitTests.Int64ExtensionsTests();
+
+
+        //--- Public Methods ---
+
+        [Test]
+        public void HighDWord()
+        {
+            testObject.HighDWord();
+        }
+
+        [Test]
+        public void LowDWord()
+        {
+            testObject.LowDWord();
+        }
+
+        [Test]
+        public void RotateLeft()
+        {
+            testObject.RotateLeft();
+        }
+
+        [Test]
+        public void RotateRight()
+        {
+            testObject.RotateRight();
+        }
+    }
+}
--- NUnitTests.NLib_NET3/Properties/AssemblyInfo.cs
+++ NUnitTests.NLib_NET3/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLib")]
+[assembly: AssemblyTitle("NUnitTests.NLib.NET3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NLib")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("NUnitTests.NLib.NET3")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("67d80075-f562-4f81-bc6f-5ace8a8d4160")]
+[assembly: Guid("c95a39c4-698c-4e49-a0b9-30cf483d88d1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
