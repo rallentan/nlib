d47745fccd2997e03cea0414bf8f32ee5aa56bd1
Mark <Mark@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2010-11-18 03:28:00 +0000

    Merged changes from the v1.1 branch into the trunk.

---
+++ trunk/NUnitTests.All/NLib_NET2/ByteExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnitTests.NLib_NET2
+{
+    [TestFixture]
+    public class ByteExtensionsTests
+    {
+        //--- Fields ---
+
+        NUnitTests.ByteExtensionsTests testObject = new NUnitTests.ByteExtensionsTests();
+
+
+        //--- Public Methods ---
+
+        [Test]
+        public void HighNibble()
+        {
+            testObject.HighNibble();
+        }
+
+        [Test]
+        public void LowNibble()
+        {
+            testObject.LowNibble();
+        }
+
+        [Test]
+        public void RotateLeft()
+        {
+            testObject.RotateLeft();
+        }
+
+        [Test]
+        public void RotateRight()
+        {
+            testObject.RotateRight();
+        }
+    }
+}
---
+++ trunk/NUnitTests.Common/Int32ExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NLib;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class Int32ExtensionsTests
+    {
+        //--- Constants ---
+
+        const int TEST_VALUE = 0x12B9B0A1;
+        const short HIGH_WORD = unchecked((short)0x12B9);
+        const short LOW_WORD = unchecked((short)0xB0A1);
+        const int ROTATE_COUNT = 3;
+        const int ROR_VALUE = unchecked((int)0x22573614);
+        const int ROL_VALUE = unchecked((int)0x95CD8508);
+        const int BIT_SIZE = 32;
+
+
+        //--- Public Methods ---
+
+        [Test]
+        public void HighWord()
+        {
+            Assert.AreEqual(HIGH_WORD, TEST_VALUE.HighWord());
+        }
+
+        [Test]
+        public void LowWord()
+        {
+            Assert.AreEqual(LOW_WORD, TEST_VALUE.LowWord());
+        }
+
+        [Test]
+        public void RotateLeft()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { TEST_VALUE.RotateLeft(-1); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { TEST_VALUE.RotateLeft(BIT_SIZE + 1); });
+            Assert.AreEqual(TEST_VALUE, TEST_VALUE.RotateLeft(0));
+            Assert.AreEqual(TEST_VALUE, TEST_VALUE.RotateLeft(BIT_SIZE));
+            Assert.AreEqual(ROL_VALUE, TEST_VALUE.RotateLeft(ROTATE_COUNT));
+        }
+
+        [Test]
+        public void RotateRight()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => { TEST_VALUE.RotateRight(-1); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { TEST_VALUE.RotateRight(BIT_SIZE + 1); });
+            Assert.AreEqual(TEST_VALUE, TEST_VALUE.RotateRight(0));
+            Assert.AreEqual(TEST_VALUE, TEST_VALUE.RotateRight(BIT_SIZE));
+            Assert.AreEqual(ROR_VALUE, TEST_VALUE.RotateRight(ROTATE_COUNT));
+        }
+    }
+}
