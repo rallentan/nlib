90b8585411cc5b151d2b68875eb497593465bbe9
drifter <drifter@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2010-11-19 19:50:10 +0000

    Added descriptions for the NLib assemblies.

--- branches/v1.1/NLib (.NET4 CP)/Properties/AssemblyInfo.cs
+++ branches/v1.1/NLib (.NET4 CP)/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides an API which extends the .NET 4 Client Profile Framework.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLib")]
