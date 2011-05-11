668242119746078a20794b21b72922d154114e48
drifter <drifter@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2011-05-11 09:23:43 +0000

    (Helper.cs, VerboseStringArray.cs) - Fixed 'using' issues to allow compile.

--- trunk/NUnitTests.NLib (Common)/StringExtensionsTests/Helper.cs
+++ trunk/NUnitTests.NLib (Common)/StringExtensionsTests/Helper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using NLib;
+using System.Threading;
+using System.Globalization;
 
 namespace NUnitTests.NLib.StringExtensionsTests
 {
