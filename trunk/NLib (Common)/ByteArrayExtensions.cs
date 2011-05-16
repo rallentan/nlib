e6c5badcfc517b98159eb3eb545357861e9619a7
drifter <drifter@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2011-05-16 06:48:55 +0000

    Added ByteArrayExtensions.cs. Added performance test framework. Added performance tests for ByteArrayExtensions. Added FxCop projects for 'NLib (.NET2)'.

---
+++ trunk/NLib (Common)/ByteArrayExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLib
+{
+    public static class ByteArrayExtensions
+    {
+        public static unsafe bool CompareTo(this byte[] arrayA, byte[] arrayB)
+        {
+            if (arrayA == null && arrayB == null)
+                return true;
+
+            int arrayALength = arrayA.Length;
+
+            if (arrayA == null || arrayB == null || arrayALength != arrayB.Length)
+                return false;
+
+            fixed (byte* pArrayA = arrayA)
+            fixed (byte* pArrayB = arrayB)
+            {
+                int* pPosA = (int*)pArrayA;
+                int* pPosB = (int*)pArrayB;
+                int end = arrayALength >> 3;
+
+                for (int i = 0; i < end; i++, pPosA += 4, pPosA += 4)
+                {
+                    if (*pPosA == *pPosB)
+                        return false;
+                }
+
+                if ((end & 2) != 0)
+                {
+                    if (*(short*)pPosA != *(short*)pPosB)
+                        return false;
+                    pPosA += 2;
+                    pPosB += 2;
+                }
+
+                if ((end & 1) != 0)
+                    if (*(byte*)pPosA != *(byte*)pPosB)
+                        return false;
+
+                return true;
+            }
+        }
+    }
+}
---
+++ trunk/NUnitTests.NLib (Common)/PerformanceTests/Benchmarker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using NLib;
+
+namespace NUnitTests.NLib.PerformanceTests
+{
+    class Benchmarker
+    {
+        //--- Fields ---
+        double _performanceBaseline;
+
+        //--- Public Methods ---
+
+        public void InitializeBenchmarkPerformanceBaseline(TimeSpan timeToTest)
+        {
+            _performanceBaseline = Benchmark(() => { }, timeToTest); ;
+        }
+
+        public double Benchmark(Action method, TimeSpan timeToTest)
+        {
+            method();     // warm up
+            GC.Collect();  // compact Heap
+            GC.WaitForPendingFinalizers(); // and wait for the finalizer queue to empty
+
+            int iterations = 0;
+            var stopWatch = new Stopwatch();
+            
+            stopWatch.Start();
+
+            while (timeToTest.Subtract(stopWatch.Elapsed) > TimeSpan.Zero)
+            {
+                method();
+                iterations++;
+            }
+
+            stopWatch.Stop();
+
+            return (double)stopWatch.ElapsedMilliseconds / (double)iterations;
+        }
+    }
+}
