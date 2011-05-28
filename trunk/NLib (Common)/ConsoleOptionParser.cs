f670e1c0cc0604da4c0f1a22853621bee05ef0dc
drifter <drifter@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2011-05-28 05:37:14 +0000

    (ConsoleOptionExceptions.cs, ConsoleOptionParser.cs) - Added
    (ExceptionHelper.cs)
    - Added new exception message string.

--- trunk/NLib (Common)/ConsoleOptionParser.cs
+++ trunk/NLib (Common)/ConsoleOptionParser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace NLib
 {
@@ -10,21 +12,32 @@ namespace NLib
         //--- Fields ---
         IEnumerable<ConsoleOptionSchem> _optionSchems;
         OptionInfo[] _optionInfos;
+        int _minFloatingArgs;
+        int _maxFloatingArgs;
 
         //--- Constructors ---
 
-        public ConsoleOptionParser(params ConsoleOptionSchem[] options)
-            : this((IEnumerable<ConsoleOptionSchem>)options)
+        public ConsoleOptionParser(int minFloatingArgs, int maxFloatingArgs, params ConsoleOptionSchem[] options)
+            : this(minFloatingArgs, maxFloatingArgs, (IEnumerable<ConsoleOptionSchem>)options)
         {
         }
 
-        public ConsoleOptionParser(IEnumerable<ConsoleOptionSchem> options)
+        public ConsoleOptionParser(int minFloatingArgs, int maxFloatingArgs, IEnumerable<ConsoleOptionSchem> options)
         {
+            if (options == null)
+                options = new ConsoleOptionSchem[0];
+            if (minFloatingArgs < 0)
+                throw new ArgumentOutOfRangeException("minFloatingArgs", ExceptionHelper.EXCMSG_MUST_BE_NONNEGATIVE);
+            if (maxFloatingArgs < minFloatingArgs)
+                throw new ArgumentException("Parameter must be greater than or equal to minFloatingArgs.");
+            
             _optionSchems = options;
+            _minFloatingArgs = minFloatingArgs;
+            _maxFloatingArgs = maxFloatingArgs;
 
-            _optionInfos = new OptionInfo[_optionSchems.Count()];
+            _optionInfos = new OptionInfo[_optionSchems.Count() + 1];  // The extra element contains floating args (index 0)
 
-            int i = 0;
+            int i = 1;
             foreach (var option in options)
             {
                 _optionInfos[i].OptionName = option.Name;
@@ -34,7 +47,7 @@ namespace NLib
 
         //--- Public Methods ---
 
-        public void ReadOptions(string[] options)
+        public void ParseOptions(string[] options)
         {
             int optionsLength = options.Length;
 
@@ -72,17 +85,50 @@ namespace NLib
                     goto NextOption;
                 }
 
-                throw new UnknownOptionException(option);
+                if (_optionInfos[0].SubOptions.Count >= _maxFloatingArgs)
+                    throw new UnknownOptionException(option);
+
+                _optionInfos[0].SubOptions.Add(option);
 
             NextOption:
                 ;
             }
+
+            if (_optionInfos[0].SubOptions.Count < _minFloatingArgs)
+                throw new MissingRequiredOptionsException();
         }
 
         public OptionInfo GetOptionInfo(string optionName)
         {
             return _optionInfos.Where((o) => { return o.OptionName == optionName; }).Single();
         }
+
+        [Conditional("DEV")]
+        //public
+        void WriteUsageInformation()
+        {
+            StringBuilder usageInfo = new StringBuilder();
+            string exeName = Assembly.GetEntryAssembly().GetName().Name;
+
+            usageInfo.AppendLine(UsageTitle);
+            usageInfo.Append("Usage: ").Append(exeName);
+
+            if (_optionSchems.Count() > 0)
+                usageInfo.Append(" [OPTIONS]");
+            Console.Write("Usage: " + exeName + ' ');
+        }
+
+        //--- Public Properties ---
+
+        public string UsageSyntax { get; set; }
+
+        public string UsageTitle { get; set; }
+
+        public string UsageDescription { get; set; }
+
+        public string UsageNotes { get; set; }
+
+        public string[] UsageExamples { get; set; }
 
         //--- Private Nested Types ---
 
@@ -93,9 +139,9 @@ namespace NLib
 
             //--- Public Methods ---
 
-            public string OptionName { get; set; }
+            public string OptionName { get; internal set; }
 
-            public int Count { get; set; }
+            public int Count { get; internal set; }
 
             public IList<string> SubOptions { get { return _subOptions; } }
         }
