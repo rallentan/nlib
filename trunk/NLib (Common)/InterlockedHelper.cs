47fcba88c4374020ddd413bfcc938138e5b2a0e2
drifter <drifter@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2013-01-17 23:05:42 +0000

    (InterlockedHelper.cs) - Added

---
+++ trunk/NLib (Common)/InterlockedHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace NLib
+{
+    public static class InterlockedHelper
+    {
+        [CLSCompliant(false)]
+        public static unsafe uint Decrement(ref uint location)
+        {
+            fixed (uint* uintPtr = &location)
+            {
+                int* intPtr = (int*)uintPtr;
+                return (uint)Interlocked.Decrement(ref *intPtr);
+            }
+        }
+
+        [CLSCompliant(false)]
+        public static unsafe uint Increment(ref uint location)
+        {
+            fixed (uint* uintPtr = &location)
+            {
+                int* intPtr = (int*)uintPtr;
+                return (uint)Interlocked.Increment(ref *intPtr);
+            }
+        }
+    }
+}
