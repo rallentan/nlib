7fc615a6eb8873f44ee9f91735ffb4512471e1f9
zerocrux9@gmail.com <zerocrux9@gmail.com@4a7eda16-a51f-6c84-ca0c-79cc507c1f67>
2013-01-30 02:25:04 +0000

    (StringHelper.cs) - Changed FirstIndexOfLine to return NPos when the specified line number is too high. - Fixed all methods to throw exceptions on bad input. - Added XML documentation.

--- trunk/NLib (Common)/StringHelper.cs
+++ trunk/NLib (Common)/StringHelper.cs
@@ -27,7 +27,10 @@ namespace NLib
         /// Returns the number of lines in a given string using a given newline 
         /// character sequence.
         /// </summary>
-        /// <param name="source">The <see cref="String"/> to get the line count.</param>
+        /// <param name="source">The System.String to get the line count.</param>
+        /// <returns>the number of lines in the source string.</returns>
+        /// <exception cref="System.ArgumentNullException">source is null,
+        /// -or- newLineSequence is null.</exception>
         public static int GetLineCount(string source)
         {
             return GetLineCount(source, Environment.NewLine);
@@ -39,11 +42,17 @@ namespace NLib
         /// </summary>
         /// <param name="source">The <see cref="String"/> to get the line count.</param>
         /// <param name="newLineSequence">
-        /// A <see cref="String"/> specifying which character sequence is used
+        /// A System.String specifying which character sequence is used
         /// to differentiate lines.
         /// </param>
+        /// <returns>the number of lines in the source string.</returns>
+        /// <exception cref="System.ArgumentNullException">source is null,
+        /// -or- newLineSequence is null.</exception>
         public static int GetLineCount(string source, string newLineSequence)
         {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            
             int pos = -1;
             int lineCount = 0;
             int newLineSequenceLength = newLineSequence.Length;
@@ -63,8 +72,12 @@ namespace NLib
         /// Retrieves the zero-based line number of the line containing the
         /// specified index.
         /// </summary>
-        /// <param name="source">The <see cref="String"/> to search.</param>
+        /// <param name="source">The System.String to search.</param>
         /// <param name="index">The index to retrieve the line number for.</param>
+        /// <returns>The zero-based line number of the line containing the
+        /// specified index.</returns>
+        /// <exception cref="System.ArgumentNullException">source is null,
+        /// -or- newLineSequence is null.</exception>
         /// <exception cref="System.ArgumentOutOfRangeException">
         /// index is less than zero, or index is greater than the length of
         /// the source string.</exception>
@@ -78,7 +91,7 @@ namespace NLib
         /// specified index. A parameter specifies which newline character
         /// sequence to use when differentiating lines.
         /// </summary>
-        /// <param name="source">The <see cref="String"/> to search.</param>
+        /// <param name="source">The System.String to search.</param>
         /// <param name="index">The index to retrieve the line number for.</param>
         /// <param name="newLineSequence">
         /// A <see cref="String"/> specifying which character sequence is used
@@ -86,11 +99,15 @@ namespace NLib
         /// </param>
         /// <returns>The zero-based line number of the line containing the
         /// specified index.</returns>
+        /// <exception cref="System.ArgumentNullException">source is null,
+        /// -or- newLineSequence is null.</exception>
         /// <exception cref="System.ArgumentOutOfRangeException">
         /// index is less than zero, or index is greater than the length of
         /// the source string.</exception>
         public static int LineNumberOfIndex(string source, int index, string newLineSequence)
         {
+            if (source == null)
+                throw new ArgumentNullException("source");
             if (index < 0 || index > source.Length)
                 throw new ArgumentOutOfRangeException(ExceptionHelper.ARGNAME_INDEX, ExceptionHelper.EXCMSG_INDEX_OUT_OF_RANGE);
 
@@ -113,8 +130,15 @@ namespace NLib
         /// Retrieves the index of the first character of a given
         /// zero-based line number.
         /// </summary>
-        /// <param name="source">The <see cref="String"/> to search.</param>
+        /// <param name="source">The System.String to search.</param>
         /// <param name="line">The line number to retrieve the index of.</param>
+        /// <returns>the index of the first character on the specified line, -or-
+        /// StringHelper.NPos (-1) if the specified line number is greater than
+        /// or equal to the number of lines in the string.</returns>
+        /// <exception cref="System.ArgumentNullException">source is null,
+        /// -or- newLineSequence is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// line is a negative number.</exception>
         public static int FirstIndexOfLine(string source, int line)
         {
             return FirstIndexOfLine(source, line, Environment.NewLine);
@@ -125,14 +149,25 @@ namespace NLib
         /// zero-based line number. A parameter specifies which newline character
         /// sequence to use when differentiating lines.
         /// </summary>
-        /// <param name="source">The <see cref="String"/> to search.</param>
+        /// <param name="source">The System.String to search.</param>
         /// <param name="line">The line number to retrieve the index of.</param>
         /// <param name="newLineSequence">
-        /// A <see cref="String"/> specifying which character sequence is used
+        /// A System.String specifying which character sequence is used
         /// to differentiate lines.
         /// </param>
+        /// <returns>the index of the first character on the specified line, -or-
+        /// StringHelper.NPos (-1) if the specified line number is greater than
+        /// or equal to the number of lines in the string.</returns>
+        /// <exception cref="System.ArgumentNullException">source is null,
+        /// -or- newLineSequence is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// line is a negative number.</exception>
         public static int FirstIndexOfLine(string source, int line, string newLineSequence)
         {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (line < 0)
+                throw new ArgumentOutOfRangeException("line", "Parameter must be non-negative.");
             int pos = 0;
             int lineNumber = 0;
             int newLineSequenceLength = newLineSequence.Length;
@@ -145,8 +180,9 @@ namespace NLib
                 lineNumber++;
             }
 
-            if (pos == StringHelper.NPos)
-                throw new ArgumentOutOfRangeException(ExceptionHelper.ARGNAME_LINE);
+            // Redundant
+            //if (pos == StringHelper.NPos)
+            //    pos = StringHelper.NPos;
 
             return lineNumber;
         }
